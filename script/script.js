1e077ee10492ac84011137b38f3f31c714dc7b8b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-19 20:12:23 +0100

    Working on game logics

--- script/script.js
+++ script/script.js
@@ -1,11 +1,13 @@
 const numbersRow = document.querySelector("#numbersRow");
+const inputsRow = document.querySelector("#inputsRow");
+const resultRow = document.querySelector("#resultRow");
 const timerDom = document.querySelector("#timerDom");
+const cardBtnSubmit = document.querySelector("#cardBtnSubmit");
+const playBtn = document.querySelector("#playBtn");
 
 let numbersToRemember = noRepeatNumbersGenerator(5);
-const timer = setInterval(updateTimer, 1000);
-let seconds = 30;
-
-displayNumbers(numbersToRemember, numbersRow);
+let timer;
+let seconds = 5;
 
 function randomNumberGenerator(min = 1, max = 100) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -30,10 +32,40 @@ function displayNumbers(numbersArray, place) {
   }
 }
 
-function updateTimer() {
+function updateGameTimer() {
   if (seconds === 0) {
     clearInterval(timer);
+    hideNumbers();
   }
   timerDom.innerText = `00:${seconds > 10 ? seconds : "0" + seconds}`;
   seconds--;
 }
+// function preGame(){
+//     let toGameSeconds = 3;
+//     let gameStart = setInterval(()=>{
+// document.querySelector('#resultRow > h3').innerHTML = `Game start in ${toGameSeconds}`;
+// toGameSeconds--
+//     })
+// }
+
+function startGame() {
+  timer = setInterval(updateGameTimer, 1000);
+  displayNumbers(numbersToRemember, numbersRow);
+}
+
+function hideNumbers() {
+  // Hide numbers for those who wanna cheat and watch the inspector
+  let elements = document.querySelectorAll("#numbersRow > p");
+  for (let index = 0; index < elements.length; index++) {
+    elements[index].innerHTML = "**";
+  }
+
+  //   Change the window to inputs
+  numbersRow.classList.add("d-none");
+  inputsRow.classList.remove("d-none");
+  cardBtnSubmit.classList.remove("opacity-0");
+}
+
+playBtn.addEventListener("click", () => {
+  preGame();
+});
