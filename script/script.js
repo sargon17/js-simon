14e063515a18b14cc52f23abb0c7c860f2cd5ab2
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-20 13:42:42 +0100

    Pre game waiting zone done

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 const numbersRow = document.querySelector("#numbersRow");
 const inputsRow = document.querySelector("#inputsRow");
-const resultRow = document.querySelector("#resultRow");
+const messagesRow = document.querySelector("#messagesRow");
 const timerDom = document.querySelector("#timerDom");
 const cardBtnSubmit = document.querySelector("#cardBtnSubmit");
 const playBtn = document.querySelector("#playBtn");
 
-let numbersToRemember = noRepeatNumbersGenerator(5);
+let numbersToRemember = [];
 let timer;
 let seconds = 5;
 
@@ -40,17 +40,31 @@ function updateGameTimer() {
   timerDom.innerText = `00:${seconds > 10 ? seconds : "0" + seconds}`;
   seconds--;
 }
-// function preGame(){
-//     let toGameSeconds = 3;
-//     let gameStart = setInterval(()=>{
-// document.querySelector('#resultRow > h3').innerHTML = `Game start in ${toGameSeconds}`;
-// toGameSeconds--
-//     })
-// }
+function preGame() {
+  let toGameSeconds = 3;
+  let gameStart = setInterval(() => {
+    if (toGameSeconds === 0) {
+      clearInterval(gameStart);
+      startGame();
+    }
+    document.querySelector(
+      "#messagesRow > h3"
+    ).innerHTML = `Game start in ${toGameSeconds}`;
+    toGameSeconds--;
+  }, 1000);
+}
 
 function startGame() {
-  timer = setInterval(updateGameTimer, 1000);
+  // Generated NUmbers Array
+  numbersToRemember = noRepeatNumbersGenerator(5);
+  console.log(numbersToRemember);
+  // Display numbers
+  hideFromDisplay(messagesRow);
   displayNumbers(numbersToRemember, numbersRow);
+  showToDisplay(numbersRow);
+
+  // Timer settings
+  timer = setInterval(updateGameTimer, 1000);
 }
 
 function hideNumbers() {
@@ -61,9 +75,16 @@ function hideNumbers() {
   }
 
   //   Change the window to inputs
-  numbersRow.classList.add("d-none");
-  inputsRow.classList.remove("d-none");
+  hideFromDisplay(numbersRow);
+  showToDisplay(inputsRow);
   cardBtnSubmit.classList.remove("opacity-0");
+}
+
+function hideFromDisplay(domElement) {
+  domElement.classList.add("d-none");
+}
+function showToDisplay(domElement) {
+  domElement.classList.remove("d-none");
 }
 
 playBtn.addEventListener("click", () => {
